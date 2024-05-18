cd689cb80e2f2e131e4ca3c7cb9eeb8e13848878
Govert van Drimmelen <govert@dnakode.com>
2024-05-18 11:45:42 +0200

    Add project files.

---
+++ MoreFunctions/MoreFunctions.cs
@@ -0,0 +1,10 @@
+﻿using ExcelDna.Integration;
+
+namespace MoreFunctions
+{
+    public class MoreFunctions
+    {
+        [ExcelFunction(Name = "SayHelloFromMoreFunctions", Description = "Say hello from MoreFunctions")]
+        public static object SayHelloFromMoreFunctions(string name) => $"Hello, {name} from MoreFunctions!";
+    }
+}
---
+++ TestMultiProj/Functions.cs
@@ -0,0 +1,11 @@
+﻿using ExcelDna.Integration;
+
+namespace TestMultiProj
+{
+    public class Functions
+    {
+        [ExcelFunction(Name = "SayHelloFromFunctions", Description = "Say hello from Functions")]
+        public static string SayHelloFromFunctions(string name) => $"Hello, {name} from Functions!";
+
+    }
+}
